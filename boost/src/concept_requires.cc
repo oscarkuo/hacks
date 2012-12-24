18ac0966f22d9dad31484da9780320dcac8d6026
Oscar Kuo <oscar@jag.local>
2012-12-25 11:01:24 +1300

    added binary function concept check example

--- boost/src/concept_requires.cc
+++ boost/src/concept_requires.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <boost/concept_check.hpp>
 #include <boost/lambda/lambda.hpp>
 #include <boost/concept/requires.hpp>
@@ -12,10 +13,28 @@ print_result (int value, UnaFunc func)
 	std::cout << func(value) << std::endl;
 }
 
+template<typename BinFunc>
+BOOST_CONCEPT_REQUIRES(
+	((boost::BinaryFunction<BinFunc, void, int&, int&>)),
+	(void))
+do_something_to_two_values (int& value1, int& value2, BinFunc func)
+{
+	func(value1, value2);
+}
+
 int main() {
 	using namespace boost;
 	using namespace boost::lambda;
 
 	print_result(10, (_1*2));
+
+	int value1 = 40;
+	int value2 = 80;
+
+	do_something_to_two_values(value1, value2, std::swap<int>);
+
+	std::cout << value1 << std::endl;
+	std::cout << value2 << std::endl;
+
 	return 0;
 }
